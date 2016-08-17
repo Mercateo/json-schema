231eaada5baa2533ddbaf00fc8fd6da46a321c69
Andreas Würl <andi@tryb.de>
2016-08-17 20:40:00 +0200

    fix broken import

--- src/main/java/com/mercateo/jsonschema/property/PropertyDescriptorResult.java
+++ src/main/java/com/mercateo/jsonschema/property/PropertyDescriptorResult.java
@@ -2,7 +2,6 @@ package com.mercateo.jsonschema.property;
 
 import com.mercateo.immutables.Tuple;
 import com.mercateo.jsonschema.generictype.GenericType;
-import com.sun.javaws.jnl.PropertyDesc;
 import javaslang.collection.Map;
 import org.immutables.value.Value;
 
