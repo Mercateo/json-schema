46123dd08fe558643e4dcc93c95843def50d9ebe
Andreas Würl <andi@tryb.de>
2018-04-11 18:44:39 +0200

    added more tests

--- src/test/java/com/mercateo/jsonschema/generictype/GenericArrayTest.kt
+++ src/test/java/com/mercateo/jsonschema/generictype/GenericArrayTest.kt
@@ -8,15 +8,21 @@ class GenericArrayTest {
 
     internal class TestClass<T> {
         private val values: Array<T>? = null
+        private val typedValues: Array<String>? = null
     }
 
     private lateinit var genericType: GenericType<*>
 
+    private lateinit var concreteType: GenericType<Any>
+
     @Before
     @Throws(NoSuchFieldException::class)
     fun setUp() {
-        val field = TestClass::class.java.getDeclaredField("values")
-        this.genericType = GenericType.of(field.genericType, field.type)
+        val genericField = TestClass::class.java.getDeclaredField("values")
+        this.genericType = GenericType.of(genericField.genericType, genericField.type)
+
+        val concreteField = TestClass::class.java.getDeclaredField("typedValues")
+        this.concreteType = GenericType.of(concreteField.genericType, concreteField.type)
     }
 
     @Test
@@ -34,4 +40,18 @@ class GenericArrayTest {
         assertThat(genericType.superType).isNull()
     }
 
+    @Test
+    fun deliversContainedType() {
+        assertThat(concreteType.containedType).isEqualTo(GenericType.of(String::class.java))
+    }
+
+    @Test
+    fun shouldBeIterable() {
+        assertThat(concreteType.isIterable).isTrue()
+    }
+
+    @Test
+    fun shouldNotBeEnum() {
+        assertThat(concreteType.isEnum).isFalse()
+    }
 }
