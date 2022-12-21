064cb3850694eaa7b98cd7c9ec34d56dd4793143
Andreas Wuerl <andi@tryb.de>
2022-12-21 23:38:36 +0100

    fix jvm version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-}
-
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.7.21"
-    id("com.github.kt3k.coveralls") version "2.8.4"
+    kotlin("jvm") version "1.7.21"
+    id("com.github.kt3k.coveralls") version "2.12.0"
     `java-library`
     `maven-publish`
     idea
@@ -23,8 +17,13 @@ repositories {
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
     }
 }
 
@@ -81,7 +80,7 @@ publishing {
     }
 }
 
-val jackson_version = "2.12.5"
+val jackson_version = "2.12.7"
 val jmh_version = "1.33"
 
 dependencies {
@@ -91,17 +90,17 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:${jackson_version}")
     implementation("org.slf4j:slf4j-api:1.7.32")
     implementation("javax.validation:validation-api:2.0.1.Final") {
-    exclude("testng")
-}
+        exclude("testng")
+    }
 //    implementation(group: "org.jetbrains.kotlin", name: "kotlin-stdlib", version: kotlin_version)
 //    implementation(group: "org.jetbrains.kotlin", name: "kotlin-reflect", version: kotlin_version)
 
-    testImplementation("junit:junit:4.13")
+    testImplementation("junit:junit:4.13.2")
     testImplementation("org.assertj:assertj-core:3.20.2")
     testImplementation("io.mockk:mockk:1.12.0")
     testImplementation("org.openjdk.jmh:jmh-core:${jmh_version}")
     testAnnotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:${jmh_version}")
-    testImplementation("ch.qos.logback:logback-classic:1.2.5")
+    testImplementation("ch.qos.logback:logback-classic:1.4.5")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.hibernate:hibernate-validator:6.2.0.Final")
+    testImplementation("org.hibernate:hibernate-validator:8.0.0.Final")
 }
